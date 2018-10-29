bce48d93cc5d3d6df4371ea00fb1c3921495dcf5
kranz fontanilla <me@kranznikolai.com>
2018-10-29 18:47:35 -0400

    init

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
     angular
     .module('app',['firebase'])
     .controller('mainCtrl',mainCtrl);
-    mainCtrl.$inject =['$scope','$firebaseObject']
-    function mainCtrl($scope,$firebaseObject){
+    mainCtrl.$inject =['$scope','$firebaseArray']
+    function mainCtrl($scope,$firebaseArray){
 
 
         var ref = firebase.database().ref().child('Wishes');
-        var syncObject = $firebaseObject(ref);
+        $scope.wishes = $firebaseArray(ref);
         // synchronize the object with a three-way data binding
         // click on `index.html` above to see it used in the DOM!
-        syncObject.$bindTo($scope, "data");
+       // syncObject.$bindTo($scope, "data");
+
+        $scope.add = function(){
+            $scope.wishes.$add({
+                wish:'test'
+            });
+        }
+       $scope.add();
     }
 })();
