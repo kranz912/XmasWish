5943805ae40f0b05e4aa6f962bdb86a3bf22df2a
kranz fontanilla <me@kranznikolai.com>
2018-10-29 18:55:49 -0400

    added modalService

---
+++ ModalService.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    angular.module('app')
+        .service('modalService', modalService);
+    modalService.$inject = ['$uibModal'];
+    function modalService($uibModal) {
+        var modalDefaults = {
+            backdrop: true,
+            keyboard: true,
+            modalFade: true,
+            templateUrl: 'Scripts/Components/AddMessageModal.html'
+        };
+        var modalOptions = {
+            closeButtonText: 'Close',
+            actionButtonText: 'OK',
+            headerText: '',
+            bodyText: ''
+        };
+        this.showModal = function (customModalDefaults, customModalOptions) {
+            if (!customModalDefaults) customModalDefaults = {};
+            customModalDefaults.backdrop = 'static';
+            return this.show(customModalDefaults, customModalOptions);
+        };
+        this.show = function (customModalDefaults, customModalOptions) {
+            var tempModalDefaults = {};
+            var tempModalOptions = {};
+            angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
+            angular.extend(tempModalOptions, modalOptions, customModalOptions);
+            if (!tempModalDefaults.controller) {
+                tempModalDefaults.controller = function ($scope, $uibModalInstance) {
+                    $scope.modalOptions = tempModalOptions;
+                    $scope.modalOptions.ok = function (result) {
+                        $uibModalInstance.close(result);
+                    };
+                    $scope.modalOptions.close = function (result) {
+                        $uibModalInstance.dismiss('cancel');
+                    };
+                };
+            }
+            return $uibModal.open(tempModalDefaults).result;
+
+        }
+    }
+
+})();
